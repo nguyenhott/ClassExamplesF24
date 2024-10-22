533cbc2cf15dff8798d4cc6a9332741ddfcf0630
Nguyen Ho <llmsgpt@gmail.com>
2024-10-21 20:54:21 -0400

    inheritance example

--- Inheritance/src/Tester.java
+++ Inheritance/src/Tester.java
@@ -2,7 +2,7 @@ public class Tester {
 
     public static void main(String[] args){
 
-        BankCustomer bCustomer = new BankCustomer("1234", "Nguyen Ho", "Baltimore", 1000);
+        BankCustomer bCustomer = new BankCustomer("1234", "James Smith", "Baltimore", 1000);
         bCustomer.display();
     }
 }
