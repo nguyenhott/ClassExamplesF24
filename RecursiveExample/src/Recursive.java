2744f80deeb58c3517ab6e5c07d83771b6ef3756
Nguyen Ho <nguyen.ho.tt@gmail.com>
2024-10-25 14:51:51 -0400

    Recursive example

--- RecursiveExample/src/Recursive.java
+++ RecursiveExample/src/Recursive.java
@@ -1,12 +1,21 @@
 public class Recursive {
 
     public static void main(String[] args){
-        int result = power(2,100000);
+        double result = power(2,100000);
     }
 
-    public static int power(int base, int exponent){
+    public static double power(double base, int exponent){
         if(exponent == 0) return 1;
         return base * power(base, exponent -1);
+    }
+
+    public static double powerIter(double base, int exponent){
+        double result = 1;
+        for(int i = 0; i < exponent; i++)
+        {
+            result = result * base;
+        }
+        return result;
     }
 
 }
